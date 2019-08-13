40dfa765221f6c928f7e933e448ff5b93540ec34
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-08-13 15:56:58 +0200

    update files

--- reconstruct.py
+++ reconstruct.py
@@ -7,64 +7,62 @@ from sim_one_ica import singlesubica
 from split_concatenate import split_concatenate
 from randomP import randomP
 from groupicas import groupicas
+from computeA import computeA
 
 import numpy as np
 from sklearn.decomposition import FastICA
 
 
-P = randomP(6, 2)
+P = randomP(6, 3)
 P
 
 data = singlesubica()
 x1 = data['Xe']
+data = singlesubica()
 x2 = data['Xe']
+data = singlesubica()
 x3 = data['Xe']
 data = singlesubica()
 x4 = data['Xe']
+data = singlesubica()
 x5 = data['Xe']
+data = singlesubica()
 x6 = data['Xe']
+
 
 data = np.stack([x1,x2,x3,x4,x5,x6])
 np.shape(data)
 
-data = split_concatenate(data, P)
-
-X1 = data[0]
-X2 = data[1]
-np.shape(X1)
-np.shape(X2)
+datasplit = split_concatenate(data, P)
 
 nc = 2
 
-[S,A] = groupicas(data, P, nc=2)
+[S,A] = groupicas(datasplit, P, nc=2)
+np.shape(A)
 A
+np.shape(S)
+
 import numpy as np
 
 def reconstruct(data, S, A, P):
+
     shape = np.shape(P)
+    M = np.shape(S)[1]
+    N = np.shape(A)
+    N
+    Ahats = np.zeros(shape = (shape[0], shape[1], N[1], N[2]))
+    #Xhats = np.zeros(shape = (shape[0], shape[1], M, N[1]))
+    Lir = np.zeros(shape = shape)
 
-    # sum A over arrays
-    At = np.sum(A, axis=0)
 
-
-    Xhat = np.zeros(shape)
 
     for i in range(shape[0]):
-        np.shape(S)
 
+        for j in range(shape[1]):
+            A = computeA(data[i], S[j])
+            Ahats[i, j, :, :] = A
 
+            Xhat = np.dot(S[j], A.T)
 
-
-
-
-
-
-
-
-
-test = np.arange((3*4*3)).reshape((3,4,3))
-test
-np.sum(test,0)
-
-A
-At
+            Loss = np.sum(np.power( (data[i] - Xhat), 2 ))
+            Lir[i,j] = Loss
