7558655d38b4f7863e8048965dea92e1260c8835
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-08-14 08:42:29 +0200

    Add empty cluster check to reestimateP code

--- reestimateP.py
+++ reestimateP.py
@@ -12,6 +12,9 @@ from computeA import computeA
 import numpy as np
 from sklearn.decomposition import FastICA
 
+### print suppress scientific notation
+np.set_printoptions(suppress=True)
+
 '''
 P = randomP(6, 3)
 P
@@ -49,18 +52,23 @@ def reestimateP(data, S, A, P):
     This function updates the P matrix
     Input:  data = original datasets
             S = ica estimated components
-            A =
+            A = mixing matrices
+    Output: new estimated P matrix
 
     '''
     shape = np.shape(P)
-    #M = np.shape(S)[1]
     N = np.shape(A)
 
     Ahats = np.zeros(shape = (shape[0], shape[1], N[1], N[2]))
-    #Xhats = np.zeros(shape = (shape[0], shape[1], M, N[1]))
     Lir = np.zeros(shape = shape)
     newP = np.zeros(shape = shape, dtype=int)
 
+    # outer for loop per subject
+    # then inner loop per cluster
+    # compute Xhat based on computing Ai with different S matrix
+    # compute loss function ||Xi - Xhat||2
+    # put in NxC loss matrix
+    # compute new P matrix by checking minimal loss per subject
     for i in range(shape[0]):
 
         for j in range(shape[1]):
@@ -72,8 +80,51 @@ def reestimateP(data, S, A, P):
             Loss = np.sum( np.power( (data[i] - Xhat), 2) )
             Lir[i,j] = Loss
 
+
+
     nP = np.argmin(Lir, axis = 1)
+
     for p in range(shape[0]):
         newP[p, nP[p]] = 1
+
+    newP
+    newP[5,2]=0
+
+    # check for empty clusters
+    # use while loop for check multiple empty clusters
+    emcluscount = np.sum(newP, axis = 0)
+    flag = any(emcluscount == 0)
+
+    # add flag iteration and warning if this code does not work
+    # just to escape the while loop
+
+    flagiter = 0
+    while flag == True:
+        flagiter += 1
+        #index of empty clusters
+        emptycluster = emcluscount == 0
+
+        # get loss function values
+        minn = np.zeros(shape=(shape[0]))
+
+        for n in range(shape[0]):
+            minn[n] = Lir[n, nP[n]]
+
+        # get top loss values indices to swap
+        idtoswap = minn.argsort()[-np.sum(emptycluster):][::-1]
+
+        for swap in range(np.sum(emptycluster)):
+            # put old 1 to 0
+            oldone = newP[idtoswap[swap],:] == 1
+            newP[idtoswap[swap],oldone] = 0
+            # put emtpy clus to 1
+            idemp = np.where(emptycluster)[0]
+            replaceid = np.random.choice(idemp,1)
+            newP[idtoswap[swap], replaceid] = 1
+
+        emcluscount = np.sum(newP, axis = 0)
+        flag = any(emcluscount == 0)
+        if flagiter == 30:
+            flag = False
 
     return newP
